5354ec43d1bb4a75e0a05a0aee427465af03ca04
asifshezan <asifshezan7@gmail.com>
2024-01-10 23:47:41 +0600

    Vue.js Events

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+ function doSomething(){
+  console.log('doSomething');
+ }
+</script>
+
+<template>
+  <section>
+    <h1>Events Vue.js</h1>
+    <!-- <form action="https://google.com/search"> -->
+      <div class="div">
+        <input @click="console.log('World')" type="text" name="q"/>
+      <button @click="console.log('hi')" class="bg-red-500">
+        Do Something
+      </button>
+      </div>
+    <!-- </form> -->
+  </section>
+</template>
+
+<style scoped>
+
+</style>
